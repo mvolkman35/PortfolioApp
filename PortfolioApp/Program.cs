cfd733bfe505bacf83dec96eb5566773e78d1cf6
Mike.Volkman <michael.j.volkman@gmail.com>
2021-01-31 14:34:23 -0600

    Creating first button

--- PortfolioApp/Program.cs
+++ PortfolioApp/Program.cs
@@ -17,7 +17,7 @@ namespace PortfolioApp
             // Testing Git
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PortfolioPresentation());
         }
     }
 }
